c179c76887e6e02ae8a01653d92552f0dcdf24b5
Ryan McCarty <rmac_whf@yahoo.com>
2017-10-27 11:51:28 -0400

    checking in some stuff

---
+++ LetsJam/Models/ViewModels/RequestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsJam.Models.ViewModels
+{
+    public class RequestViewModel
+    {
+        public List<Relation> Relation { get; set; }
+        public ApplicationUser AppUser { get; set; }
+    }
+}
---
+++ LetsJam/Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsJam.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public List<Band> Band { get; set; }
+   
+        public ApplicationUser User { get; set; }
+
+        public Relation relation { get; set; }
+
+        public bool checkconnectedbutnotconfirmed { get; set; }
+        public bool checkConnectedAndConfirmed { get; set; }
+        public bool checkConnectedAndConfirmed2 { get; set; }
+        public List<Relation> friendList { get; set; }
+        public List<Relation> friendList2 { get; set; }
+        public List<Relation> friendList3 { get; set; }
+        public List<Relation> friendList4 { get; set; }
+        public IEnumerable<Band> Currentbands { get; set; }
+      
+    }
+}
