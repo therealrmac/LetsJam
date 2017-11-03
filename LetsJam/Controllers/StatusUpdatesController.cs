3eda4a7dcfa35c77761d4ce0833807c56033a4c1
Ryan McCarty <rmac_whf@yahoo.com>
2017-11-03 17:28:03 -0400

    persoal update messages

---
+++ LetsJam/Controllers/StatusUpdatesController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LetsJam.Data;
+using LetsJam.Models;
+using Microsoft.AspNetCore.Identity;
+using LetsJam.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LetsJam.Controllers
+{
+    public class StatusUpdatesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StatusUpdatesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        // GET: StatusUpdates
+        public async Task<IActionResult> Index()
+        {
+            var messageVM = new MessageViewModel();
+
+            var currentuser = await GetCurrentUserAsync();
+
+            messageVM.updates = await _context.StatusUpdates.Include(m => m.message).Include(u => u.user).Where(x => x.user == currentuser).ToListAsync();
+
+            return View(messageVM);
+        }
+
+        // GET: StatusUpdates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statusUpdates = await _context.StatusUpdates
+                .SingleOrDefaultAsync(m => m.updateId == id);
+            if (statusUpdates == null)
+            {
+                return NotFound();
+            }
+
+            return View(statusUpdates);
+        }
+
+        // GET: StatusUpdates/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StatusUpdates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("updateId,message,created")] StatusUpdates statusUpdates)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(statusUpdates);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(statusUpdates);
+        }
+
+        // GET: StatusUpdates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statusUpdates = await _context.StatusUpdates.SingleOrDefaultAsync(m => m.updateId == id);
+            if (statusUpdates == null)
+            {
+                return NotFound();
+            }
+            return View(statusUpdates);
+        }
+
+        // POST: StatusUpdates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("updateId,message,created")] StatusUpdates statusUpdates)
+        {
+            if (id != statusUpdates.updateId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(statusUpdates);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StatusUpdatesExists(statusUpdates.updateId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(statusUpdates);
+        }
+
+        // GET: StatusUpdates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statusUpdates = await _context.StatusUpdates
+                .SingleOrDefaultAsync(m => m.updateId == id);
+            if (statusUpdates == null)
+            {
+                return NotFound();
+            }
+
+            return View(statusUpdates);
+        }
+
+        // POST: StatusUpdates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var statusUpdates = await _context.StatusUpdates.SingleOrDefaultAsync(m => m.updateId == id);
+            _context.StatusUpdates.Remove(statusUpdates);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool StatusUpdatesExists(int id)
+        {
+            return _context.StatusUpdates.Any(e => e.updateId == id);
+        }
+    }
+}
---
+++ LetsJam/Migrations/20171103212523_third.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LetsJam.Data.Migrations
+{
+    public partial class third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusUpdates",
+                columns: table => new
+                {
+                    updateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    created = table.Column<DateTime>(nullable: false, defaultValueSql: "GETDATE()"),
+                    message = table.Column<string>(nullable: true),
+                    userId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusUpdates", x => x.updateId);
+                    table.ForeignKey(
+                        name: "FK_StatusUpdates_AspNetUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusUpdates_userId",
+                table: "StatusUpdates",
+                column: "userId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusUpdates");
+        }
+    }
+}
